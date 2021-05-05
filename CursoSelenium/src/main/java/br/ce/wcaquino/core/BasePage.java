39be40b7c0b42450a4b16e339a9535a5a34b908a
Erik Lima <eriklim@hotmail.com>
2021-05-05 16:17:14 -0300

    commit-curso-selenium-05-05-2021

--- CursoSelenium/src/main/java/br/ce/wcaquino/core/BasePage.java
+++ CursoSelenium/src/main/java/br/ce/wcaquino/core/BasePage.java
@@ -1,3 +1,5 @@
+//Original author: Francisco Wagner Costa Aquino (with adaptations)
+//Contact: https://www.udemy.com/user/francisco-wagner-costa-aquino/
 package br.ce.wcaquino.core;
 
 public class BasePage {
--- ProjetoBasico/src/main/java/br/ce/wcaquino/tests/ContaTest.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/tests/ContaTest.java
@@ -1,3 +1,5 @@
+//Original author: Francisco Wagner Costa Aquino (with adaptations)
+//Contact: https://www.udemy.com/user/francisco-wagner-costa-aquino/
 package br.ce.wcaquino.tests;
 
 import org.junit.Assert;
